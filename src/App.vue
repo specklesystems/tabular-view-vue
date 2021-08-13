b2329feded81cb857b36274563e7978d398c59bf
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-08-13 15:19:20 +0300

    feat: simpler template initialisation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      Hello Speckle!
+    </v-app-bar>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,148 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+
+    <div class="mb-4">
+      <v-text-field label="Object Url" v-model="url"></v-text-field>
+      <v-text-field label="Limit" v-model.number="limit" type="number"></v-text-field>
+      <v-text-field label="Select" v-model="select"></v-text-field>
+      <v-text-field label="Query" v-model="query"></v-text-field>
+      
+      <v-btn elevation="2" color="primary" :loading='fetchLoading && !prevLoading && !nextLoading' @click="fetchChildren">Fetch</v-btn>
+    </div>
+
+    <p class="caption">Total count: {{totalCount ? totalCount : 'unknown'}}</p>
+    
+    <v-data-table :headers="headers" :items="flatObjs" :items-per-page="limit" hide-default-footer class="elevation-1 my-4"></v-data-table>
+    
+    <v-btn @click="prev" :loading='prevLoading' :disabled="cursors.length <= 2" class="mr-2">prev</v-btn>
+    <v-btn @click="next" :loading='nextLoading' :disabled="cursors.length === 0 || (cursors.length - 1) * limit >= totalCount" class="mr-2">next</v-btn>
+    
+    <p class="caption mt-2">Curr items: {{ ( cursors.length - 1 ) * limit }} Cursor: {{cursors ? cursors : 'n/a'}}</p>
+  </v-container>
 </template>
 
 <script>
+import flat from 'flat'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {},
+  data() {
+    return {
+      url: 'https://speckle.xyz/streams/0d3cb7cb52/objects/f833afec2e17457f17e7f6429a106187',
+      totalCount: null,
+      query: '[{"field":"speckle_type","operator":"=","value":"Objects.BuiltElements.Wall:Objects.BuiltElements.Revit.RevitWall"}]',
+      select: '["speckle_type","id","parameters.HOST_AREA_COMPUTED.value", "parameters.HOST_VOLUME_COMPUTED.value"]',
+      cursors: [],
+      flatObjs: [],
+      headers: [],
+      limit: 10,
+      fetchLoading: false,
+      prevLoading: false,
+      nextLoading: false
+    }
+  },
+  watch: {
+    limit() {
+      // If the limit is changed, we need to reset the query
+      this.fetchChildren()
+    }
+  },
+  methods: {
+    async next() {
+      this.nextLoading = true
+      await this.fetchChildren( false, this.cursors[ this.cursors.length - 1 ] )
+      this.nextLoading = false
+    },
+    async prev() {
+      this.prevLoading = true
+      await this.cursors.pop() // remove last cursor
+      await this.fetchChildren( false, this.cursors[ this.cursors.length - 2 ], false ) // fetch using the second last cursor
+      this.prevLoading = false
+    },
+
+    async fetchChildren( cleanCursor = true, cursor = null, appendCursor = true ) {
+
+      // Parse the object's url and extract the info we need from it.
+      const url = new URL( this.url )
+      const server = url.origin
+      const streamId = url.pathname.split( '/' )[2]
+      const objectId = url.pathname.split( '/' )[4]
+
+      // Get the gql query string. 
+      const query = this.getQuery( streamId, objectId, cursor )
+      
+      // Set loading status
+      this.fetchLoading = true
+
+      // Send the request to the Speckle graphql endpoint.
+      // Note: The limit, selection and query clause are passed in as variables.
+      let rawRes = await fetch( new URL( '/graphql', server ), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {
+          query: query,
+          variables: {
+            limit: this.limit,
+            mySelect: this.select ? JSON.parse( this.select ) : null, 
+            myQuery: this.query ? JSON.parse( this.query ) : null
+          }
+        } )
+      } )
+      
+      // Parse the response into. 
+      let res = await rawRes.json()
+      
+      let obj = res.data.stream.object
+      
+      this.totalCount = obj.children.totalCount
+
+      // Cursor management. 
+      if( cleanCursor ) this.cursors = [ null ]
+      if( appendCursor ) this.cursors.push( obj.children.cursor ) 
+
+      // Flatten the objects! 
+      this.flatObjs = obj.children.objects.map( o => flat( o.data, { safe: false } ) )
+
+      // Create a unique list of all the headers.
+      const uniqueHeaderNames = new Set()
+      this.flatObjs.forEach( o => Object.keys( o ).forEach( k => !k.includes( '__closure' ) ? uniqueHeaderNames.add( k ) : null ) )
+      
+      this.headers = []
+      uniqueHeaderNames.forEach( val => this.headers.push( { text: val, value: val, sortable: false } ) )
+
+      // Last, signal that we're done loading!
+      this.fetchLoading = false
+    },
+
+    getQuery( streamId, objectId, cursor = null ) {
+      return `
+        query( $limit: Int, $mySelect: [String!], $myQuery: [JSONObject!]) {
+          stream( id: "${streamId}" ) {
+            object( id: "${objectId}" ) {
+              data
+              children( 
+                limit: $limit
+                depth: 100
+                select: $mySelect
+                query: $myQuery
+                ${ cursor ? ', cursor:"' + cursor + '"' : '' }
+                ) {
+                totalCount
+                cursor
+                objects {
+                  id
+                  data
+                }
+              }
+            }
+          }
+        }
+      `
+    }
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
